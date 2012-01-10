99bb1ad2e658c00b961320196b0eb293aa31d591
xbsd <xbsd.nikolay@gmail.com>
2012-01-11 00:43:45 +0400

    Fix manifest, add icon and create context menu item

--- back.js
+++ back.js
@@ -1,5 +1,19 @@
 
-function onClick(dataInfo, callback){
-		
-}
+var c = function (onClickInfo, tab){
 
+	console.log("Hello, world!")
+		
+	text = onClickInfo.selectionText;
+	chrome.tabs.create({
+		url: "http://translate.google.com/"
+	});
+};
+
+console.log("Hello, world!")
+
+chrome.contextMenus.create({
+	title: "Translate %s to Ru",
+	contexts: ["selection"],
+	onclick: c
+});
+console.log("Hello, world!")
